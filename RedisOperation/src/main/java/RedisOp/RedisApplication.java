0696fe9dbe06913ddb83559cf4e5133ba7abfcf0
liuchenyang01234 <921971242@qq.com>
2019-07-15 20:01:44 +0800

    添加Redsi

--- RedisOperation/src/main/java/RedisOp/RedisApplication.java
+++ RedisOperation/src/main/java/RedisOp/RedisApplication.java
@@ -1,19 +1,15 @@
-package kafkade;
+package RedisOp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @ImportResource({ "classpath:*.xml" })
-public class Application4{
+public class RedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application4.class, args);
+        SpringApplication.run(RedisApplication.class, args);
     }
 
 }
